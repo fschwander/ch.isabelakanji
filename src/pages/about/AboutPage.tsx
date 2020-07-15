7720e0eccff1c9b8fdf4282d7d674f85fb8788b8
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-07-15 23:33:21 +0200

    implemented AboutPage.tsx and added some styling

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './AboutPage.scss';
+import portraitImage from '../../res/imgs/portrait_round.jpg';
 
-interface AboutPageProps {
-  windowHeight: string
-}
-
-export const AboutPage: React.FC<AboutPageProps> = props => {
+export const AboutPage: React.FC = () => {
 
   return (
-    <div className={'AboutPage'} style={{minHeight: props.windowHeight}}>
+    <div className={'AboutPage'}>
+      <div className={'image-container'}>
+        <img src={portraitImage} alt={'Portrait'}/>
+        <a className={'contact-button button'} href={'mailto:yoga@isabelakanji.ch'}>
+          <p>Kontakt</p>
+        </a>
+      </div>
 
+      <h1>Isabel Akanji</h1>
+
+      <p>Diplomierte Yogalehrerin</p>
+      <p>Diplomierte Heilkräuterfrau</p>
+      <p>Kinderskilehrerin</p>
+
+      <p>Wohnhaft in Savognin und am Zürichsee</p>
     </div>
   );
 };
