6ee2ece6735fa748bcf5de5bc649f5aecf5e24cb
Fabian Schwander <fabian.schwander@students.fhnw.ch>
2020-07-22 13:48:10 +0200

    bug fix: added div as container in order to scroll and display page properly

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -6,22 +6,24 @@ export const AboutPage: React.FC = () => {
 
   return (
     <div className={'AboutPage'}>
-      <div className={'image-container'}>
-        <img src={portraitImage} alt={'Portrait'}/>
-        <a className={'contact-button button'} href={'mailto:yoga@isabelakanji.ch'}>
-          <p>Kontakt</p>
-        </a>
+      <div>
+        <div className={'image-container'}>
+          <img src={portraitImage} alt={'Portrait'}/>
+          <a className={'contact-button button'} href={'mailto:yoga@isabelakanji.ch'}>
+            <p>Kontakt</p>
+          </a>
+        </div>
+
+        <h1>Isabel Akanji</h1>
+
+        <div className={'list-container'}>
+          <p>Diplomierte Yogalehrerin</p>
+          <p>Diplomierte Heilkräuterfrau</p>
+          <p>Kinderskilehrerin</p>
+        </div>
+
+        <p>Wohnhaft in Savognin und am Zürichsee</p>
       </div>
-
-      <h1>Isabel Akanji</h1>
-
-      <div className={'list-container'}>
-        <p>Diplomierte Yogalehrerin</p>
-        <p>Diplomierte Heilkräuterfrau</p>
-        <p>Kinderskilehrerin</p>
-      </div>
-
-      <p>Wohnhaft in Savognin und am Zürichsee</p>
     </div>
   );
 };
