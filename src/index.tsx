88a441c0b5f84fed35360dc886f36e382ed4d428
fschwander <fschwander@gmx.ch>
2024-12-30 16:14:13 +0100

    deployment script; npm updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './services/serviceWorker';
-// @ts-ignore
-import {render} from 'react-snapshot';
-
-render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {createRoot} from "react-dom/client";
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App/>)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
